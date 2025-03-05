7a272f705fd593ce9b319c4becb84c7fed2edece
kvfs2k07 <64465478+kvfs2k07@users.noreply.github.com>
2025-03-06 00:35:52 +0100

    Lets pray it works first time (idek what I'm doing)

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+image_name=kvfs2k07/nodejs-w-php
+image_versions="1.0"
+
+cd $(dirname "${0}")
+
+for image_version in ${image_versions}; do
+    echo Downloading kvfs2k07/nodejs-w-php:${image_version}
+    docker pull kvfs2k07/nodejs-w-php:${image_version}
+
+    docker rmi ${image_name}:${image_version}
+    docker rmi ${image_name}:latest
+
+    echo Building image ${image_name}:${image_version}
+    docker build \
+        --pull \
+        --build-arg http_proxy=${http_proxy} \
+        -t ${image_name}:${image_version} \
+        -t ${image_name}:latest \
+        -f ${image_version}/Dockerfile \
+        ./
+
+    echo Pushing image ${image_name}:${image_version}
+    docker push ${image_name}:${image_version}
+
+    echo Pushing image ${image_name}:latest
+    docker push ${image_name}:latest
+done
